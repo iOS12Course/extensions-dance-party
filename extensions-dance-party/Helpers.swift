84f21c17948ac3811e29dacffcdf599b3711078a
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-15 00:36:56 -0500

    extensions

--- extensions-dance-party/Helpers.swift
+++ extensions-dance-party/Helpers.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+func generateRandomNumbers(quantity: Int) -> [CGFloat] {
+    var randomNumberArray = [CGFloat]()
+    for _ in 1...quantity {
+        let randomNumber = CGFloat(arc4random_uniform(255))
+        randomNumberArray.append(randomNumber)
+    }
+    return randomNumberArray
+}
